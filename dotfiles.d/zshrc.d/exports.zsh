b67be5bc5571bda73dec577ce823fdcf55c03def
CryDeTaan <christiaan.dewet@gmail.com>
2017-08-22 08:32:43 +0200

    Fixing tmux ssh userAgent issues

--- dotfiles.d/zshrc.d/exports.zsh
+++ dotfiles.d/zshrc.d/exports.zsh
@@ -1,2 +1,7 @@
 export PATH="$HOME/.composer/vendor/bin:/home/crydetaan/.config/composer/vendor/bin:$PATH"
 export BROWSER="/usr/bin/chrome"
+
+# Fix SSH auth socket location so agent forwarding works with tmux
+if test "$SSH_AUTH_SOCK" ; then
+  ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+fi
