b46a873a4fe8e6dcc8913c10e19b9d2aa33fefc2
CryDeTaan <crydetaan@gmail.com>
2018-01-28 09:43:43 +0200

    Adding exports for brew python

--- dotfiles.d/zshrc.d/exports.zsh
+++ dotfiles.d/zshrc.d/exports.zsh
@@ -1,3 +1,6 @@
 export PATH="$HOME/.composer/vendor/bin:/home/crydetaan/.config/composer/vendor/bin:$PATH"
-export BROWSER="/usr/bin/chrome"
 
+
+# Brew installs a python2 executable to /usr/local/bin.
+# To have brew's python executable in PATH we need the following.
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
