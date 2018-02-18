78061fa91d30b294d9b35cd45fa2d8427e09524a
CryDeTaan <crydetaan@gmail.com>
2018-02-18 10:06:18 +0200

    Adding exports for brew python

--- dotfiles.d/zshrc.d/exports.zsh
+++ dotfiles.d/zshrc.d/exports.zsh
@@ -4,3 +4,8 @@ export PATH="$HOME/.composer/vendor/bin:/home/crydetaan/.config/composer/vendor/
 # Brew installs a python2 executable to /usr/local/bin.
 # To have brew's python executable in PATH we need the following.
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+
+# The following is required for the python virtualenvwrapper
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Devel
+source /usr/local/bin/virtualenvwrapper.sh
