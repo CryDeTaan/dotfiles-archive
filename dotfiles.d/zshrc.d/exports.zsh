3bb579e2d972a0cd403d9552a5d53da6d16022c8
CryDeTaan <crydetaan@gmail.com>
2018-04-08 13:38:13 +0200

    Added support for python virtualenvwrapper on CentOS

--- dotfiles.d/zshrc.d/exports.zsh
+++ dotfiles.d/zshrc.d/exports.zsh
@@ -8,4 +8,14 @@ export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 # The following is required for the python virtualenvwrapper
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Devel
-source /usr/local/bin/virtualenvwrapper.sh
+
+# Sourcing the correct virtualwrapper.sh depending on the OS
+#  MacOS
+if [ -f "/usr/local/bin/virtualenvwrapper.sh" ]; then
+    source /usr/local/bin/virtualenvwrapper.sh
+
+# CentOS
+elif [ -f "/usr/bin/virtualenvwrapper.sh" ]; then
+    source /usr/bin/virtualenvwrapper.sh
+
+fi
