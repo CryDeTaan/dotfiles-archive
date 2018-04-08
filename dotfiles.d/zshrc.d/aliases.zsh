19acd9827fcb444858b03f7bc42a00a075714403
CryDeTaan <crydetaan@gmail.com>
2018-04-08 19:22:43 +0200

    Nice mkdir and cd combination 2 :)

--- dotfiles.d/zshrc.d/aliases.zsh
+++ dotfiles.d/zshrc.d/aliases.zsh
@@ -8,4 +8,4 @@ alias zshconf="vim ~/.zshrc"
 alias ohmyzsh="vim ~/.oh-my-zsh"
 alias dotfiles="bash <(curl -fsSL https://raw.githubusercontent.com/crydetaan/dotfiles/master/dotfiles)"
 alias ss="pngpaste /tmp/pngpaste.png; curl -s -F sunset=86400 -F c=@- -F p=1 https://ptpb.pw/ < /tmp/pngpaste.png | egrep -o 'https:\/\/ptpb.pw\/.*' | pbcopy; rm /tmp/pngpaste.png"
-cdmkdir () { mkdir -p "$@" && cd "$@"; }
+mcd () { mkdir -p "$@" && cd "$@"; }
