3cae5f8515ca407084d20c7d2cc8a3c58b5200e8
CryDeTaan <crydetaan@gmail.com>
2018-04-14 10:59:18 +0200

    Whoops, I am a n00b

--- dotfiles.d/zshrc.d/exports.zsh
+++ dotfiles.d/zshrc.d/exports.zsh
@@ -7,16 +7,21 @@ which composer > /dev/null && export PATH="$HOME/.composer/vendor/bin:$PATH"
 # My OS of choice is MacOS or CentOS(most distros probably),
 
 case "$OSTYPE" in
-    darwin*) # MacOS
+    # MacOS
+    darwin*)
         # The correct virtualwrapper.sh depending on the OS
         source /usr/local/bin/virtualenvwrapper.sh
 
         # Brew installs a python2 executable to /usr/local/bin.
         # To have brew's python executable in PATH we need the following.
         export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+        ;;
 
-    linux*) # CentOS
+    # CentOS
+    linux*) 
         source /usr/bin/virtualenvwrapper.sh
+        ;;
+esac
 
 # The following is required for the python virtualenvwrapper
 which virtualenv > /dev/null && export WORKON_HOME=$HOME/.virtualenvs && export PROJECT_HOME=$HOME/Devel
