e85d26b76638261391d0c1f979237d2522a75573
CryDeTaan <crydetaan@gmail.com>
2018-05-06 13:30:49 +0200

    STFU 2

--- dotfiles.d/zshrc.d/exports.zsh
+++ dotfiles.d/zshrc.d/exports.zsh
@@ -1,16 +1,17 @@
 # Main file for setting some exports and sourcing some other files. 
 
 # Adding Composer to $PATH if running on system
-which composer 2>/dev/null && export PATH="$HOME/.composer/vendor/bin:$PATH"
+which composer > /dev/null 2>&1 && export PATH="$HOME/.composer/vendor/bin:$PATH"
 
 # Some OS specific sourcing.
 # My OS of choice is MacOS or CentOS(most distros probably),
 
+# The following is required for the python virtualenvwrapper depending on the OS.
 case "$OSTYPE" in
     # MacOS
     darwin*)
         # The correct virtualwrapper.sh depending on the OS
-        source /usr/local/bin/virtualenvwrapper.sh
+        which virtualenv > /dev/null 2>&1 && source /usr/local/bin/virtualenvwrapper.sh && export WORKON_HOME=$HOME/.virtualenvs && export PROJECT_HOME=$HOME/Devel
 
         # Brew installs a python2 executable to /usr/local/bin.
         # To have brew's python executable in PATH we need the following.
@@ -19,9 +20,6 @@ case "$OSTYPE" in
 
     # CentOS
     linux*) 
-        source /usr/bin/virtualenvwrapper.sh
+        which virtualenv > /dev/null 2>&1 && source /usr/bin/virtualenvwrapper.sh && export WORKON_HOME=$HOME/.virtualenvs && export PROJECT_HOME=$HOME/Devel
         ;;
 esac
-
-# The following is required for the python virtualenvwrapper
-which virtualenv 2>/dev/null && export WORKON_HOME=$HOME/.virtualenvs && export PROJECT_HOME=$HOME/Devel
